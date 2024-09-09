8330a59107dfee7176578ad2f542b3e7bcb72beb
vincent <jacky.vincent095@gmail.com>
2024-09-09 10:32:07 +0800

    exchange update

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
 contract Exchange {
     address public tokenAddress;
@@ -12,14 +13,74 @@ contract Exchange {
         tokenAddress = token_;
     }
 
-
     function addLiquidity(uint256 tokenAmount) public payable {
         IERC20 token = IERC20(tokenAddress);
         token.transferFrom(msg.sender, address(this), tokenAmount);
     }
 
-    function getReserve() public view returns(uint256) {
+    function getReserve() public view returns (uint256) {
         return IERC20(tokenAddress).balanceOf(address(this));
     }
 
+    function getPrice(
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) public pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "insufficent reserve");
+        return (inputReserve * 1000) / outputReserve;
+    }
+
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256) {
+        // console.log(inputAmount, inputReserve, outputReserve);
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+
+        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
+    function getTokenAmount(uint256 ethSold_) public view returns (uint256) {
+        require(ethSold_ > 0, "ethSold is too small");
+        uint256 tokenReserve = getReserve();
+        uint256 etherReserve = address(this).balance;
+        return getAmount(ethSold_, etherReserve, tokenReserve);
+    }
+
+    function getEthAmount(uint256 tokensSold_) public view returns (uint256) {
+        require(tokensSold_ > 0, "tokensSold is too small");
+        uint256 tokenReserve = getReserve();
+        uint256 etherReserve = address(this).balance;
+        return getAmount(tokensSold_, tokenReserve, etherReserve);
+    }
+
+    function ethToTokenSwap(uint256 minTokens_) public payable {
+        uint256 tokenReserve = getReserve();
+        uint256 tokensBought = getAmount(
+            msg.value,
+            address(this).balance - msg.value,
+            tokenReserve
+        );
+        require(tokensBought >= minTokens_, "insufficient output token amount");
+        IERC20(tokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+    function tokenToEthSwap(uint256 tokensSold_, uint256 minEth_) public {
+        uint256 tokenReserve = getReserve();
+        uint256 ethBought = getAmount(
+            tokensSold_,
+            tokenReserve,
+            address(this).balance
+        );
+
+        require(ethBought >= minEth_, "insufficient output amount");
+
+        IERC20(tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            tokensSold_
+        );
+        payable(msg.sender).transfer(ethBought);
+    }
 }
--- test/Exchange.ts
+++ test/Exchange.ts
@@ -2,9 +2,13 @@ import {
     time,
     loadFixture,
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { AddressLike, BigNumberish, Numeric } from "ethers";
 import { expect } from "chai";
 import hre from "hardhat";
-const toWei = (value: bigint | number) => hre.ethers.parseEther(value.toString());
+
+const toWei = (value: Numeric) => hre.ethers.parseEther(value.toString());
+const fromWei = (value: BigNumberish) => hre.ethers.formatEther(typeof value === "string" ? value : value.toString());
+const getBalance = (address: AddressLike) => hre.ethers.provider.getBalance(address);
 
 describe("Exchange", function () {
 
@@ -22,16 +26,60 @@ describe("Exchange", function () {
         const exchangeAddress = await exchange.getAddress();
         console.log("Exchange Contract deployed to address:", exchangeAddress);
 
+        await token.approve(exchangeAddress, toWei(2000));
+        await exchange.addLiquidity(toWei(2000), { value: toWei(1000) });
+
         return { token, exchange, owner, user, tokenAddress, exchangeAddress };
     }
 
     describe("addLiquidity", function () {
         it("add liquidity", async function () {
             const { token, exchange, tokenAddress, exchangeAddress } = await loadFixture(deployExchangeFixture);
-            await token.approve(exchangeAddress, toWei(200));
-            await exchange.addLiquidity(toWei(200), { value: toWei(100) });
-            expect(await hre.ethers.provider.getBalance(exchangeAddress)).to.equal(toWei(100));
-            expect(await exchange.getReserve()).to.equal(toWei(200));
+            expect(await getBalance(exchangeAddress)).to.equal(toWei(1000));
+            expect(await exchange.getReserve()).to.equal(toWei(2000));
         });
+    });
+
+
+    describe("getPrice", function () {
+        it("return correct price", async function () {
+            const { token, exchange, tokenAddress, exchangeAddress } = await loadFixture(deployExchangeFixture);
+            const tokenReserve = await exchange.getReserve();
+            const etherReserve = await getBalance(exchangeAddress);
+
+            expect(await exchange.getPrice(etherReserve, tokenReserve)).to.eq(500);
+
+            // token per ETH
+            expect(await exchange.getPrice(tokenReserve, etherReserve)).to.eq(2000);
+        })
+    })
+
+
+    describe("getTokenAmount", function () {
+        it("return correct token amount", async function () {
+            const { token, exchange, tokenAddress, exchangeAddress } = await loadFixture(deployExchangeFixture);
+            let tokensOut = await exchange.getTokenAmount(toWei(1));
+            expect(fromWei(tokensOut)).to.equal("1.998001998001998001");
+
+            tokensOut = await exchange.getTokenAmount(toWei(100));
+            expect(fromWei(tokensOut)).to.equal("181.818181818181818181");
+
+            tokensOut = await exchange.getTokenAmount(toWei(1000));
+            expect(fromWei(tokensOut)).to.equal("1000.0");
+        })
+    })
+
+    describe("getEthAmount", function () {
+        it("return correct ether amount", async function () {
+            const { token, exchange, tokenAddress, exchangeAddress } = await loadFixture(deployExchangeFixture);
+            let ethOut = await exchange.getEthAmount(toWei(2));
+            expect(fromWei(ethOut)).to.equal("0.999000999000999");
+
+            ethOut = await exchange.getEthAmount(toWei(100));
+            expect(fromWei(ethOut)).to.equal("47.619047619047619047");
+
+            ethOut = await exchange.getEthAmount(toWei(2000));
+            expect(fromWei(ethOut)).to.equal("500.0");
+        })
     })
 })
